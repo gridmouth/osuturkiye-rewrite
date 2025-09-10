6621dcc6c9ad6185dff08c3e30f705a7849ad2ac
Rozalia "Rüya" San <150453194+gridmouth@users.noreply.github.com>
2025-09-10 17:43:02 +0200

    Added login system, added swup integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 // @ts-check
-import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
-import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
+import swup from '@swup/astro';
 
-// https://astro.build/config
 export default defineConfig({
   vite: {
+      server: {
+        allowedHosts: [ 'mwvlhn-ip-109-233-92-75.tunnelmole.net' ],
+      },
       plugins: [tailwindcss()],
   },
 
   output: "server",
-  integrations: [react()],
+  integrations: [swup()],
 });
